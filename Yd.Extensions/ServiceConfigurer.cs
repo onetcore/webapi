e4858e5ee88cd0a9cba80bc1615aa543c474157f
ztang <tangzibing@xmydcl.com>
2020-10-13 04:15:44 +0800

    修改配置注册服务。

---
+++ Yd.Extensions/ServiceConfigurer.cs
@@ -0,0 +1,21 @@
+﻿using Gentings;
+using Gentings.Extensions.Settings;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Yd.Extensions
+{
+    /// <summary>
+    /// 注册当前登录用户。
+    /// </summary>
+    public class ServiceConfigurer : IServiceConfigurer
+    {
+        /// <summary>
+        /// 配置服务方法。
+        /// </summary>
+        /// <param name="builder">容器构建实例。</param>
+        public void ConfigureServices(IServiceBuilder builder)
+        {
+            builder.AddScoped(ss => ss.GetRequiredService<ISettingsManager>().GetSettings<SiteSettings>());
+        }
+    }
+}
