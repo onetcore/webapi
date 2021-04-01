54f58f97e6ffdb9be415872694a9cd0825f877d8
ztang <tangzibing@xmydcl.com>
2021-04-01 22:25:20 +0800

    添加资源嵌入资源中引用，需要引用程序集Gentings.AspNetCore

---
+++ Yd.AspNetCore.Storages/ServiceConfigurer.cs
@@ -0,0 +1,20 @@
+﻿using Gentings;
+using Gentings.AspNetCore;
+
+namespace Yd.AspNetCore.Storages
+{
+    /// <summary>
+    /// 服务配置。
+    /// </summary>
+    public class ServiceConfigurer : IServiceConfigurer
+    {
+        /// <summary>
+        /// 配置服务方法。
+        /// </summary>
+        /// <param name="builder">容器构建实例。</param>
+        public void ConfigureServices(IServiceBuilder builder)
+        {
+            builder.AddResources<ServiceConfigurer>();
+        }
+    }
+}
